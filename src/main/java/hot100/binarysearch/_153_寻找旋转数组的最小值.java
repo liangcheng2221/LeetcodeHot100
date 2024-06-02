c4286160c4bef1665381fe581e312eedc3a35f44
yinjie <2279703188@qq.com>
2024-06-02 08:02:36 +0800

    二分基本刷完，还有一些地方不是很理解

---
+++ src/main/java/hot100/binarysearch/_153_寻找旋转数组的最小值.java
@@ -0,0 +1,39 @@
+package hot100.binarysearch;
+
+/**
+ * @author yinjie
+ * @date 2024-06-02 5:56
+ */
+public class _153_寻找旋转数组的最小值 {
+    /**
+     * 思路：如果两边都是升序的数组，就是最小的那个就是（要求log n 的时间复杂度）
+     *
+     * @param nums 反转后的数组
+     * @return 返回数组中的最小值
+     */
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (right >= left) {
+            int mid = (right + left) / 2;
+            if (mid == nums.length - 1) {
+                return nums[mid];
+            }
+            // 左边有序
+            if (nums[left] <= nums[mid]) {
+                // 右边也有序
+                if (nums[mid + 1] <= nums[right]) {
+                    return Math.min(nums[mid + 1], nums[left]);
+                } else {
+                    // 右边无序
+                    left = mid + 1;
+                }
+            } else {
+                // 左边无序，直接左边
+                right = mid;
+            }
+        }
+        // 这个返回值需不需要都无所谓
+        return 0;
+    }
+}
--- src/main/java/hot100/binarysearch/_33_搜索旋转排序数组.java
+++ src/main/java/hot100/binarysearch/_33_搜索旋转排序数组.java
@@ -5,7 +5,40 @@ package hot100.binarysearch;
  * @date 2024-05-17 16:11
  */
 public class _33_搜索旋转排序数组 {
+    // TODO 还是有点难度，不知道是不是因为很久没刷算法题了。
     public int search(int[] nums, int target) {
-        return 0;
+        if (nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
+            return target == nums[0] ? 0 : -1;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        // 左指针大于右指针直接退出
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target){
+                return mid;
+            }
+            // 左边有序
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] < target){
+                    right = mid - 1;
+                }else {
+                    left = mid;
+                }
+            } else {
+                // 右边有序，且不在右边数组
+                if (nums[mid] > target){
+                    right = mid - 1;
+                }else {
+                    left = mid;
+                }
+            }
+        }
+        return -1;
     }
 }

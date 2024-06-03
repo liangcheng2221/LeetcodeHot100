2b9515d942e779e2da103fd5b8f621d10486f148
yinjie <2279703188@qq.com>
2024-06-03 10:42:09 +0800

    这题有点小难度

---
+++ src/main/java/hot100/binarysearch/_4_寻找两个正序数组的中位数.java
@@ -0,0 +1,21 @@
+package hot100.binarysearch;
+
+/**
+ * @author yinjie
+ * @date 2024-06-03 10:26
+ */
+public class _4_寻找两个正序数组的中位数 {
+    /**
+     * 满足题意要牵扯一定的数学理解（二分查找）
+     * 1. 不要满足题意的两种做法
+     * 1.1 合并数组直接找中位数
+     * 1.2 双指针（比第一种少了空间复杂度）
+     *
+     * @param nums1 数组一
+     * @param nums2 数组二
+     * @return 返回中位数
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0;
+    }
+}

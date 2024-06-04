de26bdb4db1f0412761a4c268872db45285ddde3
yinjie <2279703188@qq.com>
2024-06-04 13:42:41 +0800

    完成20，155题。 20：简单栈操作 155：需要利用辅助栈记录每次进栈的最小值

---
+++ src/main/java/hot100/stack/_155_最小栈.java
@@ -0,0 +1,33 @@
+package hot100.stack;
+
+import java.util.Stack;
+
+/**
+ * @author yinjie
+ * @date 2024-06-04 13:01
+ */
+public class _155_最小栈 {
+    Stack<Integer> stack =new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+    public _155_最小栈() {
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        minStack.push(Math.min(minStack.peek(),val));
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
---
+++ src/main/java/hot100/stack/_20_有效括号.java
@@ -0,0 +1,41 @@
+package hot100.stack;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * @author yinjie
+ * @date 2024-06-04 12:43
+ */
+public class _20_有效括号 {
+    public static final Map<Character, Character> map = new HashMap<Character, Character>() {{
+        put('{', '}');
+        put('[', ']');
+        put('(', ')');
+    }};
+
+    /**
+     * 思路：用 map 记录对应的括号，数组元素为键，直接插入，否则判断 是否栈为空或者栈头对应
+     *
+     * @param s 字符串
+     * @return 是否为有效括号
+     */
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] charArray = s.toCharArray();
+        for (char c : charArray) {
+            // 如果为 map 键，直接入栈
+            if (map.containsKey(c)) {
+                stack.push(c);
+            } else {
+                // 不是，且栈为空或者栈头不和数组对应直接返回false
+                if (stack.isEmpty() || !map.get(stack.pop()).equals(c)) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}

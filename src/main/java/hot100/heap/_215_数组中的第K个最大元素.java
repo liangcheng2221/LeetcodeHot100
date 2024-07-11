14306060b72e205b0e5c8b58a68a0068d19582a0
yinjie <2279703188@qq.com>
2024-07-11 11:20:18 +0800

    完成_215_数组中的第K个最大元素，利用API的默认快排的排序算法

---
+++ src/main/java/hot100/heap/_215_数组中的第K个最大元素.java
@@ -0,0 +1,24 @@
+package hot100.heap;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+/**
+ * @author yinjie
+ * @date 2024-06-11 9:52
+ */
+public class _215_数组中的第K个最大元素 {
+    public int findKthLargest(int[] nums, int k) {
+        return 0;
+    }
+
+    /**
+     * 直接利用数组的排序算法（默认是快速排序的算法）
+     *
+     * @param nums 数组
+     */
+    private static void sortBySelf(int[] nums) {
+        Arrays.sort(nums);
+    }
+}

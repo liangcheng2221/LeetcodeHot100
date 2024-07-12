0659ca8a6a43a07bbe01d829f52ed28d1120b01d
yinjie <2279703188@qq.com>
2024-07-12 09:49:17 +0800

    完成_55_跳跃游戏。思路也是局部最优推出全局最优，但不是很明显（贪心）

---
+++ src/main/java/hot100/greedy/_55_跳跃游戏.java
@@ -0,0 +1,22 @@
+package hot100.greedy;
+
+/**
+ * @author yinjie
+ * @date 2024-07-12 9:26
+ */
+public class _55_跳跃游戏 {
+    public boolean canJump(int[] nums) {
+        int min = nums[0];
+        int i = 0;
+        for (; i < nums.length; i++) {
+            if (min > 0 && --min < nums[i]){
+                min = nums[i];
+                continue;
+            }
+            if (min <= 0){
+                return i == nums.length - 1;
+            }
+        }
+        return true;
+    }
+}

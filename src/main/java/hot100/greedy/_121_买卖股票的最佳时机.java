a8bd8480bf31ce12bd80612bc8c5532aa55a2363
yinjie <2279703188@qq.com>
2024-07-12 09:23:11 +0800

    完成_121_买卖股票的最佳时机。利用局部最优推出全局最优（贪心的思想）

---
+++ src/main/java/hot100/greedy/_121_买卖股票的最佳时机.java
@@ -0,0 +1,58 @@
+package hot100.greedy;
+
+import java.util.Stack;
+
+/**
+ * @author yinjie
+ * @date 2024-07-12 8:47
+ */
+public class _121_买卖股票的最佳时机 {
+
+    /**
+     * 单调栈的解题思路
+     *
+     * @param prices 价格数组
+     * @return 返回最大利润
+     */
+    public int maxProfit(int[] prices) {
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+        for (int price : prices) {
+            if (stack.isEmpty()) {
+                stack.push(price);
+                continue;
+            }
+            if (price <= stack.peek()) {
+                stack.push(price);
+                continue;
+            }
+            Integer pop = stack.pop();
+            max = Math.max(max, price - pop);
+            stack.push(price);
+            stack.push(pop);
+        }
+        return max;
+    }
+    /**
+     * 基于单调栈的优化
+     *
+     * @param prices 价格数组
+     * @return 返回最大利润
+     */
+    public int maxProfitByGreedy(int[] prices) {
+        int tempMin = Integer.MAX_VALUE;
+        int max = 0;
+
+        for (int price : prices){
+            // 遇到小的就交换，否则依旧记录最大值
+            if (tempMin > price){
+                tempMin = price;
+            }else {
+                max = Math.max(max,price - tempMin);
+            }
+        }
+
+
+        return max;
+    }
+}

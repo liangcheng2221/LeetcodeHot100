6b01b866e83ed4abd0dbe40063028a5453e0f920
yinjie <2279703188@qq.com>
2024-07-20 21:27:26 +0800

    完成_45_跳跃游戏2题，主要是贪心思路

--- src/main/java/hot100/greedy/_45_跳跃游戏2.java
+++ src/main/java/hot100/greedy/_45_跳跃游戏2.java
@@ -5,7 +5,31 @@ package hot100.greedy;
  * @date 2024-07-12 9:55
  */
 public class _45_跳跃游戏2 {
-    public int jump(int[] nums) {
-        return 0;
+    public static void main(String[] args) {
+        System.out.println(jump(new int[]{1,2,3}));
+    }
+
+    public static int jump(int[] nums) {
+        if (nums.length == 1){
+            return 0;
+        }
+        int count = 1;
+        int max = nums[0];
+        int maxIndex = 0;
+        while ((maxIndex + max) < nums.length - 1) {
+            int maxIndex1 = maxIndex;
+            int max1 = max;
+            for (int j = maxIndex1; j <= max1 + maxIndex1; j++) {
+                // 如果碰见大的记录一下，并且跟换最大值
+                if (nums[j] > max){
+                    maxIndex = j;
+                    max = nums[j];
+                }
+                max--;
+            }
+            max = nums[maxIndex];
+            count++;
+        }
+        return count;
     }
 }

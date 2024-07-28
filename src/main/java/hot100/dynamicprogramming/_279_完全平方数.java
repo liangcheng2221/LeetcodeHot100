fd6e772336aa2c0cf3e5a96d34572e0afd17c70f
yinjie <2279703188@qq.com>
2024-07-28 22:26:14 +0800

    完成_279_完全平方数，原理还没有理解，好像用到了二维dp

---
+++ src/main/java/hot100/dynamicprogramming/_279_完全平方数.java
@@ -0,0 +1,22 @@
+package hot100.dynamicprogramming;
+
+import java.util.Arrays;
+
+/**
+ * @author <a href="https://github.com/liangcheng2221">yinjie</a>
+ * @date 2024-07-27 10:24
+ */
+public class _279_完全平方数 {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i * i <= n; i++) {
+            for (int j = i * i; j <= n; j++) {
+                dp[j] = Math.min(dp[j], dp[j - i * i] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
